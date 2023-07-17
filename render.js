c8481117078ccae45452e116599d358ef2a38d24
Numa Canedo <numacanedo@gmail.com>
2023-07-17 00:04:34 -0400

    Adding text resume for online importers

--- render.js
+++ render.js
@@ -3,8 +3,12 @@ const moment = require('moment');
 const path = require('path');
 const docs = 'docs';
 const prettify = require('@liquify/prettify');
+const handlebars = require("handlebars");
 
 let resume = fileSystem.readFileSync('resume.json');
+
+console.log('Register helpers...');
+registerHelpers();
 
 console.log('Rendering resume...');
 let html = prettify.formatSync(render(resume), {
@@ -23,12 +27,13 @@ if (!fileSystem.existsSync(docs)) {
 console.log('Persisting resume...');
 fileSystem.writeFileSync(path.join(docs, 'index.html'), html);
 
+console.log('Creating text...');
+fileSystem.writeFileSync(path.join(docs, 'resume.txt'), text(resume));
+
 console.log('Creating pdf...');
 pdf(path.join(docs, 'resume.pdf'), html);
 
 function render(resume) {
-    const handlebars = require('handlebars');
-
     fileSystem
         .readdirSync('partials')
         .forEach(function (filename) {
@@ -37,31 +42,14 @@ function render(resume) {
                 fileSystem.readFileSync(path.join('partials', filename), 'utf8'));
         });
 
-    handlebars.registerHelper('format', function (startDate, endDate) {
-        return `${format(startDate)} - ${format(endDate)}`;
-    });
-
-    handlebars.registerHelper('duration', function (startDate, endDate) {
-        let duration = calculateDuration(startDate, endDate);
-
-        return `${pad(duration.years(), 'y')}${pad(duration.months(), 'm')}|`;
-    });
-
-    handlebars.registerHelper('experience', function (work) {
-        let duration = calculateDuration(work.at(-1).startDate);
-        let months = duration.months() > 5 ? ` ${duration.months()} months` : '';
-
-        return duration.years() > 0
-            ? `${duration.years()} years${months} of experience`
-            : '';
-    });
-
-    handlebars.registerHelper('add-dot', function (text) {
-        return '.' === text.slice(-1) ? text : text + '.';
-    });
-
     return handlebars.compile(fileSystem.readFileSync('resume.hbs', 'utf-8'))({
         css: fileSystem.readFileSync('style.css', 'utf-8'),
+        resume: JSON.parse(resume)
+    });
+}
+
+function text(resume) {
+    return handlebars.compile(fileSystem.readFileSync('resume-plain.hbs', 'utf-8'))({
         resume: JSON.parse(resume)
     });
 }
@@ -91,6 +79,31 @@ function pdf(pdfFile, resume) {
         console.log('Closing browser...');
         await browser.close();
     })();
+}
+
+function registerHelpers() {
+    handlebars.registerHelper('format', function (startDate, endDate) {
+        return `${format(startDate)} - ${format(endDate)}`;
+    });
+
+    handlebars.registerHelper('duration', function (startDate, endDate) {
+        let duration = calculateDuration(startDate, endDate);
+
+        return `${pad(duration.years(), 'y')}${pad(duration.months(), 'm')}|`;
+    });
+
+    handlebars.registerHelper('experience', function (work) {
+        let duration = calculateDuration(work.at(-1).startDate);
+        let months = duration.months() > 5 ? ` ${duration.months()} months` : '';
+
+        return duration.years() > 0
+            ? `${duration.years()} years${months} of experience`
+            : '';
+    });
+
+    handlebars.registerHelper('add-dot', function (text) {
+        return '.' === text.slice(-1) ? text : text + '.';
+    });
 }
 
 function calculateDuration(startDate, endDate) {
